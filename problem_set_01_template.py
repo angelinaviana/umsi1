b3497dd133809db12e5d90841fe0e353228f9571
Angelina Viana <112649943+angelinaviana@users.noreply.github.com>
2022-11-23 12:33:51 -0500

    Add files via upload

---
+++ problem_set_01_template.py
@@ -0,0 +1,86 @@
+# START PROBLEM SET 1
+print('Problem Set 1 \n')
+
+# PROBLEM 01 (10 points)
+print('\nPROBLEM 01')
+
+1frita_batidos = 'Frita Batidos'
+zingerman's Delicatessen = "Zingerman's Delicatessen"
+nypd = New York Pizza Depot
+hopcat = 'Hopcat'
+fleetwood_diner = "Fleetwood Diner"
+tomukun_noodle_bar = 'Tomukun Noodle Bar
+jerk_pit = "Jamaican Jerk Pit"
+mama satto = 'Mama Satto'
+hola_seoul = 'Hola Seoul"
+@shalimar = 'Shalimar'
+
+cottage_inn = None
+print(cottage_inn)
+
+madras_masala = None
+print(madras_masala)
+
+# PROBLEM 02 (30 points)
+print('\nPROBLEM 02')
+
+hopcat_all_upper = None
+print(f"all_upper: {hopcat_all_upper}")
+
+jerk_pit_all_lower = None
+print(f"all_lower: {jerk_pit_all_lower}")
+
+madras_masala_count_a = None
+print(f"number of letter a: {madras_masala_count_a}")
+
+has_pizza = None
+print(has_pizza)
+
+has_madras = None
+print(has_madras)
+
+comment = 'Truly authentic Jamaican food & drinks'
+updated_comment = None
+print(f"updated comment: {updated_comment}")
+
+# PROBLEM 03 (30 points)
+print('\nPROBLEM 03')
+
+num_chars = None
+print(f"number of characters: {num_chars}")
+
+restaurants = ['Frita Batidos',
+               "Zingerman's Delicatessen",
+               'New York Pizza Depot',
+               'Hopcat',
+               'Fleetwood Diner',
+               'Tomukun Noodle Bar',
+               'Jamaican Jerk Pit',
+               'Mama Satto',
+               'Hola Seoul',
+               'Shalimar',
+               'Cottage Inn Pizza',
+               'Madras Masala'
+               ]
+
+# TODO: print the type of the variable restaurants
+
+
+num_restaurants = None
+print(f"number of restaurants: {num_restaurants}")
+
+total_price = None
+print(f"total price: {total_price}")
+
+total_bill = None
+print(f"total bill: {total_bill}")
+
+each_pay = None
+print(f"each person pays: {each_pay}")
+
+# PROBLEM 04 (5 points)
+print('\nPROBLEM 04')
+
+# TODO: Use f-string to print out the sentence
+
+# END PROBLEM SET
